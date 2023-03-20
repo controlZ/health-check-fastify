48f1307d461f9c2657e4a161ed5cd6269d89e93e
controlZ <o_dahyun@gm.gist.ac.kr>
2023-03-21 03:23:28 +0900

    💎 style: remove comment

--- src/plugin/health_check.plugin.ts
+++ src/plugin/health_check.plugin.ts
@@ -1,6 +1,5 @@
 import {FastifyPluginAsync} from "fastify";
 
-//health check api
 const health_check_plugin: FastifyPluginAsync = async (fastify) => {
     fastify.get('/health', async (request, reply) => {
         const ok = {
